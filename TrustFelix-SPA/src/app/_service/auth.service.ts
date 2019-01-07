075bc0b8e9942fd4a08a5e8b412c557c3aece8e4
unknown <lanzerstorfer@web.de>
2019-01-07 16:35:42 +0100

    End of Section 4

--- TrustFelix-SPA/src/app/_service/auth.service.ts
+++ TrustFelix-SPA/src/app/_service/auth.service.ts
@@ -19,6 +19,10 @@ login(model: any) {
         localStorage.setItem('token', user.token);
       }
     })
-  )
+  );
+}
+
+register(model: any) {
+  return this.http.post(this.baseUrl + 'register', model);
 }
 }
--- TrustFelix.API/Controllers/ValuesController.cs
+++ TrustFelix.API/Controllers/ValuesController.cs
@@ -24,6 +24,7 @@ namespace TrustFelix.API.Controllers
             this._context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
