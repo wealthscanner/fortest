c6d8335bb30f4088ae39277213013a227ac440f5
unknown <lanzerstorfer@web.de>
2019-01-08 20:04:00 +0100

    lubunu migration

--- TrustFelix-SPA/src/app/nav/nav.component.ts
+++ TrustFelix-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_service/auth.service';
+import { AltertifyService } from '../_service/altertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,26 +10,25 @@ import { AuthService } from '../_service/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AltertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+      this.alertify.success('Logged in successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 }
