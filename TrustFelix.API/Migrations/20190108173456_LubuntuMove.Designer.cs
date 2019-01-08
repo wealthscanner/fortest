da2a26f9a58465246b5aa32c2e046a31f91f8a1e
wealthscanner <forex1998@gmail.com>
2019-01-08 20:08:57 +0100

    changes in Lubuntu

--- TrustFelix.API/Migrations/20190108173456_LubuntuMove.Designer.cs
+++ TrustFelix.API/Migrations/20190108173456_LubuntuMove.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrustFelix.API.Data;
 
 namespace TrustFelix.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190108173456_LubuntuMove")]
+    partial class LubuntuMove
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TrustFelix.API/Migrations/20190108173456_LubuntuMove.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrustFelix.API.Migrations
+{
+    public partial class LubuntuMove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "UserName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+        }
+    }
+}
