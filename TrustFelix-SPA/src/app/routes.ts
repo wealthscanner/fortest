2023b6b5dd2c431ef7d2ee0203d0850ea38b3988
wealthscanner <forex1998@gmail.com>
2019-01-16 10:18:13 +0100

    interim

--- TrustFelix-SPA/src/app/routes.ts
+++ TrustFelix-SPA/src/app/routes.ts
@@ -7,6 +7,8 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 export const appRoutes: Routes =  [
     { path: '', component: HomeComponent },
@@ -14,8 +16,12 @@ export const appRoutes: Routes =  [
         path: '', runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},   /* members */
-            { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},   /* /id..Details component */
+            { path: 'members', component: MemberListComponent,
+                resolve: {users: MemberListResolver}},   /* members */
+            { path: 'members/:id', component: MemberDetailComponent,
+                resolve: {user: MemberDetailResolver}},   /* /id..Details component */
+            { path: 'member/edit', component: MemberEditComponent,
+                resolve: {user: MemberEditResolver}},
             { path: 'myasset', component: MyAssetComponent },
             { path: 'messages', component: MessagesComponent }
         ]
